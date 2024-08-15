188774112ff90b6c5d7a75edfba358e5657a2845
Chris Gambrell <christopher@gambrell.info>
2024-08-14 22:08:53 -0400

    Try to fix timezone issues

--- components/schema-card.tsx
+++ components/schema-card.tsx
@@ -1,5 +1,6 @@
 import { addGeneration, deleteGeneration, deleteSchema } from '@/lib/actions'
 import { Field, Generation, Prisma, Schema } from '@prisma/client'
+import { format } from 'date-fns'
 import { EditIcon, EllipsisIcon, EyeIcon, PlusIcon, RefreshCcw, Trash2Icon } from 'lucide-react'
 import { ActionButton } from './action-button'
 import { ConfirmDelete } from './confirm-delete'
@@ -115,7 +116,7 @@ function GenerationsList({ generations, schemaName }: { generations: Array<Gener
 			<div className='grid divide-y'>
 				{generations.map((gen) => (
 					<div key={gen.id} className='text-sm py-3 first:pt-0 last:pb-0 flex items-center justify-between gap-4'>
-						<div>{new Date(gen.createdAt).toLocaleString()}</div>
+						<div>{format(gen.createdAt, 'yyyy-MM-dd h:mm:ss a')}</div>
 						<div>{gen.data.length} item(s)</div>
 						<div className='flex items-center gap-1'>
 							<GenerationDialog generation={gen} schemaName={schemaName}>
