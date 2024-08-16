c8313a7ec3b9459f8589b10358da04006d83fa03
Chris Gambrell <christopher@gambrell.info>
2024-08-16 08:21:42 -0400

    Add environment variables to workflow

--- lib/env.ts
+++ lib/env.ts
@@ -3,18 +3,20 @@ import { z } from 'zod'
 
 export const env = createEnv({
 	server: {
+		AUTH_SECRET: z.string().min(1),
 		OPENAI_API_KEY: z.string().min(1),
 		POSTGRES_URL: z.string().url(),
 		POSTGRES_PRISMA_URL: z.string().url(),
 		POSTGRES_URL_NO_SSL: z.string().url(),
 		POSTGRES_URL_NON_POOLING: z.string().url(),
 		POSTGRES_USER: z.string().min(1),
-		POSTGRES_HOST: z.string().min(1),
+		POSTGRES_HOST: z.string().url(),
 		POSTGRES_PASSWORD: z.string().min(1),
 		POSTGRES_DATABASE: z.string().min(1),
 	},
 	client: { NEXT_PUBLIC_SITE_URL: z.string().url() },
 	runtimeEnv: {
+		AUTH_SECRET: process.env.AUTH_SECRET,
 		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 		POSTGRES_URL: process.env.POSTGRES_URL,
 		POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
