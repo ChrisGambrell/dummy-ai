788a15d37e2c652a4579f1c132df70aa89c2e00c
Chris Gambrell <christopher@gambrell.info>
2024-08-22 11:04:18 -0400

    Add user dropdown and icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import { logout } from '@/actions/auth'
-import { ActionButton } from '@/components/action-button'
+import icon from '@/app/icon.png'
 import { EmptyState } from '@/components/empty-state'
 import { SchemaCard } from '@/components/schema-card'
 import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { UpsertSchema } from '@/components/upsert-schema'
 import { auth } from '@/lib/auth'
 import prisma from '@/lib/db'
-import { LogOutIcon, PlusIcon } from 'lucide-react'
+import { LogOutIcon, PlusIcon, User2Icon } from 'lucide-react'
+import Image from 'next/image'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -22,19 +31,35 @@ export default async function RootPage() {
 
 	return (
 		<div className='grid gap-4 md:gap-6 max-w-3xl mx-auto my-4 md:my-8 px-4'>
-			<div className='ml-auto flex items-center gap-2'>
-				<UpsertSchema>
-					<Button>
-						<PlusIcon className='size-4 mr-2' />
-						Add schema
-					</Button>
-				</UpsertSchema>
-				<form action={logout}>
-					<ActionButton variant='destructive'>
-						<LogOutIcon className='size-4 mr-2' />
-						Logout
-					</ActionButton>
-				</form>
+			<div className='flex gap-4 items-end'>
+				<Image className='rounded-lg size-10 sm:size-12' src={icon} width={40} height={40} alt='Dummy AI' />
+				<div className='ml-auto flex items-center gap-3'>
+					<UpsertSchema>
+						<Button>
+							<PlusIcon className='size-4 mr-2' />
+							Add schema
+						</Button>
+					</UpsertSchema>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant='outline' size='icon' className='rounded-full flex items-center justify-center'>
+								<User2Icon className='size-5' />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align='end'>
+							<DropdownMenuLabel>My Account</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem asChild>
+								<form action={logout}>
+									<button className='w-full text-left flex items-center' type='submit'>
+										<LogOutIcon className='size-4 mr-2' />
+										Logout
+									</button>
+								</form>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
 			</div>
 			{schemas.length > 0 ? (
 				schemas.map((schema) => <SchemaCard key={schema.id} schema={schema} />)
