36c3d5e24657469b8e606f48e10c06936e80cb49
Chris Gambrell <christopher@gambrell.info>
2024-08-27 10:10:11 -0400

    Ability to copy generated data

--- components/generation-dialog.tsx
+++ components/generation-dialog.tsx
@@ -1,7 +1,10 @@
 import { Generation, Schema } from '@prisma/client'
+import { FilesIcon } from 'lucide-react'
 import { ReactNode } from 'react'
+import { CopyButton } from './copy-button'
 import { DrawerDialog } from './drawer-dialog'
 import { FormatDate } from './format-date'
+import { Button } from './ui/button'
 
 export function GenerationDialog({
 	children,
@@ -22,7 +25,15 @@ export function GenerationDialog({
 				</>
 			}
 			trigger={children}>
-			<pre className='text-wrap'>{JSON.stringify(generation.data, null, 2)}</pre>
+			<div className='grid gap-2'>
+				<CopyButton value={JSON.stringify(generation.data, null, 2)}>
+					<Button className='md:w-fit'>
+						<FilesIcon className='size-4 mr-2' />
+						<span>Copy data</span>
+					</Button>
+				</CopyButton>
+				<pre className='text-wrap'>{JSON.stringify(generation.data, null, 2)}</pre>
+			</div>
 		</DrawerDialog>
 	)
 }
